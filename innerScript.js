94eea8bd3ee06a99031d54bfbab76d748cc51bda
Cláudio <claudiomeireles555@gmail.com>
2024-02-04 21:15:40 +0000

    Update

--- innerScript.js
+++ innerScript.js
@@ -1,13 +1,9 @@
 window.addEventListener("message", (event) => {
-    console.log("Message Received!");
-    console.log(event);
-    console.log(event?.data);
     if (
         event.source === window &&
         event?.data?.direction === "from-content-script" &&
         event?.data?.message === 'search'
     ) {
-        console.log("Message Valid!");
         __doPostBack('ctl00$MainContent$cmdRptSearch', '');
     }
 });
