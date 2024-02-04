dcc49c848fe73093ef7274636db7cf72653a79b3
Cláudio <claudiomeireles555@gmail.com>
2024-02-04 21:12:29 +0000

    Update

--- innerScript.js
+++ innerScript.js
@@ -1,5 +1,7 @@
 window.addEventListener("message", (event) => {
     console.log("Message Received!");
+    console.log(event);
+    console.log(event?.data);
     if (
         event.source === window &&
         event?.data?.direction === "from-page-script" &&
