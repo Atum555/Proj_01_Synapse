2638cf630bf56a74d6b124f7de8696e3e4199ba2
Cláudio <claudiomeireles555@gmail.com>
2024-02-04 20:07:04 +0000

    Update

--- contentScript.js
+++ contentScript.js
@@ -45,6 +45,25 @@ chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
     }
 });
 
+(() => {
+    const myJavaScript = `
+function handleResponse(response) {
+    console.log(response);
+    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+        const activeTab = tabs[0];
+        chrome.scripting.executeScript({
+            'target': { 'tabId': activeTab.id },
+            'func': () => {
+                __doPostBack('ctl00$MainContent$cmdRptSearch', '');
+            }
+        });
+    });
+}`;    //You need to put your JS here. 
+    const scriptTag = document.createElement("script");
+    scriptTag.innerHTML = myJavaScript;
+    document.head.appendChild(scriptTag);
+})();
+
 /* // GET DATA
 rows = document.querySelector("#ctl00_MainContent_pnlReports").querySelector(".table-body-report").children[0].children[0].children[0].children;
 
--- popup/popup.js
+++ popup/popup.js
@@ -1,16 +1,3 @@
-function handleResponse(response) {
-    console.log(response);
-    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-        const activeTab = tabs[0];
-        chrome.scripting.executeScript({
-            'target': { 'tabId': activeTab.id },
-            'func': () => {
-                __doPostBack('ctl00$MainContent$cmdRptSearch','');
-            }
-        });
-    });
-}
-
 function sendData() {
     chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
         const year = document.getElementsByClassName('year-btn-active')[0].innerText;
