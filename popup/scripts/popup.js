02d76032095fe66c0048168e87a35362ed303d16
Cláudio <claudiomeireles555@gmail.com>
2024-02-07 11:54:11 +0000

    Update

--- popup/scripts/popup.js
+++ popup/scripts/popup.js
@@ -8,7 +8,22 @@ let extensionGlobalData = {
 
 // Send Message Asking for Data
 function askData() {
-    chrome.runtime.sendMessage('send-data');
+    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+        const activeTab = tabs[0];
+        const request = 'send-data';
+        chrome.tabs.sendMessage(activeTab.id, request)
+            .catch((error) => {
+                // Set Invalid Page
+                const totalElem = document.getElementById('table-total');
+                const warningElem = document.getElementById('content-warning');
+                const tableElem = document.getElementById('content-table');
+
+                totalElem.innerText = '0000';
+                tableElem.style.display = 'none';
+                warningElem.innerText = 'WebSite inválido.';
+                warningElem.style.display = '';
+            });
+    });
 }
 setInterval(askData, 1000);
 
