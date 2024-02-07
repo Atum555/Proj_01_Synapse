94236864f85825dfcb39e4186971db6596113c91
Cláudio <claudiomeireles555@gmail.com>
2024-02-07 11:58:10 +0000

    Update

--- popup/scripts/popup.js
+++ popup/scripts/popup.js
@@ -11,18 +11,32 @@ function askData() {
     chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
         const activeTab = tabs[0];
         const request = 'send-data';
-        chrome.tabs.sendMessage(activeTab.id, request)
-            .catch((error) => {
-                // Set Invalid Page
-                const totalElem = document.getElementById('table-total');
-                const warningElem = document.getElementById('content-warning');
-                const tableElem = document.getElementById('content-table');
 
-                totalElem.innerText = '0000';
-                tableElem.style.display = 'none';
-                warningElem.innerText = 'WebSite inválido.';
-                warningElem.style.display = '';
-            });
+        // Send If Valid Page
+        if (tabs[0].url.match('https://cwm.trofasaude.com/*')) {
+            chrome.tabs.sendMessage(activeTab.id, request)
+                .catch((error) => {
+                    // Handle Error
+                    const totalElem = document.getElementById('table-total');
+                    const warningElem = document.getElementById('content-warning');
+                    const tableElem = document.getElementById('content-table');
+
+                    totalElem.innerText = '0000';
+                    tableElem.style.display = 'none';
+                    warningElem.innerText = 'Erro.';
+                    warningElem.style.display = '';
+                });
+        } else {
+            // Set Invalid Page
+            const totalElem = document.getElementById('table-total');
+            const warningElem = document.getElementById('content-warning');
+            const tableElem = document.getElementById('content-table');
+
+            totalElem.innerText = '0000';
+            tableElem.style.display = 'none';
+            warningElem.innerText = 'WebSite inválido.';
+            warningElem.style.display = '';
+        }
     });
 }
 setInterval(askData, 1000);
